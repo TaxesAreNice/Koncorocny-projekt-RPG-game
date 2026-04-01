1a543590a8fbb566e0da21f7866b3b3b88479407
mahutm25 <mahutm25@Z00T0-08>
2026-04-01 13:08:36 +0200

    don't forget da changes

--- Koncoročný projekt, RPG game/MainWindow.xaml.cs
+++ Koncoročný projekt, RPG game/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Koncoročný_projekt__RPG_game.UI_Generations;
 
 namespace Koncoročný_projekt__RPG_game
 {
@@ -16,9 +17,31 @@ namespace Koncoročný_projekt__RPG_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<List<Map_Block>> Map = [];
+      
         public MainWindow()
         {
             InitializeComponent();
+
+
+          
+
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            List<Map_Block> row = [];
+
+            for (int i = 0; i < 1; i++)
+            {
+                Map_Block block = new Map_Block();
+
+                row.Add(block);
+                
+                
+                
+                Map_UI.Children.Add(block);
+            }
         }
     }
 }
---
+++ Koncoročný projekt, RPG game/UI Generations/Map_Block.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Koncoročný_projekt__RPG_game.UI_Generations
+{
+    internal class Map_Block : Grid
+    {
+        private Grid Box;
+        private int box_position = 5;
+        public Map_Block()
+        {
+            Box = new Grid()
+            {
+                Height = 100,
+                Width = 100,
+                Margin = new Thickness(5),
+                Background = Brushes.DarkGray
+            };
+
+            Height = 100;
+            Width = 1200;
+            Margin = new Thickness(2);
+            Background = Brushes.Gray;
+            HorizontalAlignment = HorizontalAlignment.Left;
+            VerticalAlignment = VerticalAlignment.Top;
+
+
+            for (int i = 0; i < 12; i++) 
+            {
+                var tempBox = new Grid()
+                {
+                    Height = 100,
+                    Width = 100,
+                    Margin = new Thickness(5), /// change somethin' here
+                    Background = Brushes.DarkGray
+                };
+
+                Children.Add(tempBox); 
+            }
+        }
+    }
+}
