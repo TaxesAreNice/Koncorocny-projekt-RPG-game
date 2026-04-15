e7095b9fe1d808d6ff6cfe467223b50d7e56be74
mcvengros66@gmail.com <mcvengros66@gmail.com>
2026-04-15 09:11:40 +0200

    r

---
+++ Koncoročný projekt, RPG game/EnemyTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koncoročný_projekt__RPG_game
+{
+    public class EnemyTemplate
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Damage { get; set; }
+    }
+}
---
+++ Koncoročný projekt, RPG game/Items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koncoročný_projekt__RPG_game
+{
+    internal class Items
+    {
+        private List<string> items = new List<string>();
+    }
+}
