8b3a380d4b4b02c68db9c096b89e99ce974cee2d
Taxes <dataxtixer@gmail.com>
2026-04-28 20:27:02 +0200

    xcxd

---
+++ Koncoročný projekt, RPG game/UI Generations/InventorySlots_Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Koncoročný_projekt__RPG_game.UI_Generations
+{
+    internal class InventorySlots_Image : Grid
+    {
+        public Image image = new Image()
+        {
+            Height = 90,
+            Width = 90,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
+        public InventorySlots_Image(int box_position, int xX, int y)
+        {
+            Height = 100;
+            Width = 100;
+            Margin = new Thickness(box_position, 5, 5, 5);
+            Name = "_" + xX.ToString() + "_" + y.ToString();
+            HorizontalAlignment = HorizontalAlignment.Left;
+
+            Children.Add(image);
+        }
+    }
+}
