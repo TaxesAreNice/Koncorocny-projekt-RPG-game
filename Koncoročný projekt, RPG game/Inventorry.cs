e5289df56cbac3790076567f4230d49ebe6e9395
mcvengros66@gmail.com <mcvengros66@gmail.com>
2026-05-20 11:29:21 +0200

    steel armor pics.

--- Koncoročný projekt, RPG game/Inventorry.cs
+++ Koncoročný projekt, RPG game/Inventorry.cs
@@ -61,7 +61,7 @@ namespace Koncoročný_projekt__RPG_game
 
             Items.Add(new Item { Name = "Knight Helmet_Helmet", Type = ItemType.Wearable, Description = "+4 defense", Defense = 4 });
             Items.Add(new Item { Name = "Knight Chestplate_Chestplate", Type = ItemType.Wearable, Description = "+6 defense", Defense = 6 });
-            Items.Add(new Item { Name = "Knight Legplates_Leggins", Type = ItemType.Wearable, Description = "+5 defense", Defense = 5 });
+            Items.Add(new Item { Name = "Knight Leggins_Leggins", Type = ItemType.Wearable, Description = "+5 defense", Defense = 5 });
             Items.Add(new Item { Name = "Knight Boots_Boots", Type = ItemType.Wearable, Description = "+4 defense", Defense = 4 });
             Items.Add(new Item { Name = "Knight Sword_Sword", Type = ItemType.Wearable, Description = "+15 attack", Attack = 13 });
 
@@ -96,7 +96,7 @@ namespace Koncoročný_projekt__RPG_game
 
             Items.Add(new Item { Name = "Rusty Helmet_Helmet", Type = ItemType.Wearable, Description = " +1 defense", Defense = 1 });
             Items.Add(new Item { Name = "Rusty Chestplate_Chestplate", Type = ItemType.Wearable, Description = " +2 defense", Defense = 2 });
-            Items.Add(new Item { Name = "Rusty Legplates_Legplates", Type = ItemType.Wearable, Description = " +2 defense", Defense = 2 });
+            Items.Add(new Item { Name = "Rusty Leggins_Leggins", Type = ItemType.Wearable, Description = " +2 defense", Defense = 2 });
             Items.Add(new Item { Name = "Rusty Boots_Boots", Type = ItemType.Wearable, Description = " +1 defense", Defense = 1 });
 
             Items.Add(new Item { Name = "Dull Blade_Sword", Type = ItemType.Wearable, Description = "Just some dull blade", Attack = 5 });
@@ -107,12 +107,12 @@ namespace Koncoročný_projekt__RPG_game
 
             Items.Add(new Item { Name = "Steel Helmet_Helmet", Type = ItemType.Wearable, Description = " +5 defense", Defense = 5 });
             Items.Add(new Item { Name = "Steel Chestplate_Chestplate", Type = ItemType.Wearable, Description = " +8 defense", Defense = 8 });
-            Items.Add(new Item { Name = "Steel Legplates_Legplates", Type = ItemType.Wearable, Description = " +7 defense", Defense = 7 });
+            Items.Add(new Item { Name = "Steel Leggins_Leggins", Type = ItemType.Wearable, Description = " +7 defense", Defense = 7 });
             Items.Add(new Item { Name = "Steel Boots_Boots", Type = ItemType.Wearable, Description = " +5 defense", Defense = 5 });
 
             Items.Add(new Item { Name = "Iron Helmet_Helmet", Type = ItemType.Wearable, Description = " +3 defense", Defense = 3 });
             Items.Add(new Item { Name = "Iron Chestplate_Chestplate", Type = ItemType.Wearable, Description = " +5 defense", Defense = 5 });
-            Items.Add(new Item { Name = "Iron Legplates_Leggins", Type = ItemType.Wearable, Description = " +4 defense", Defense = 4 });
+            Items.Add(new Item { Name = "Iron Leggins_Leggins", Type = ItemType.Wearable, Description = " +4 defense", Defense = 4 });
             Items.Add(new Item { Name = "Iron Boots_Boots", Type = ItemType.Wearable, Description = " +3 defense", Defense = 3 });
         }
     }
